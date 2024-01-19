4acd5c7ecaf1aefe6adafd76fe21f7b63c516d75
Paola931 <145440087+Paola931@users.noreply.github.com>
2024-01-19 10:41:29 +0100

    Update ProgrammingLanguageRepository.java

--- programming_app/programming_app/src/main/java/com/example/programming_app/ProgrammingLanguageRepository.java
+++ programming_app/programming_app/src/main/java/com/example/programming_app/ProgrammingLanguageRepository.java
@@ -5,8 +5,5 @@ import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-//(path = "/repo-prog-languages",collectionResourceDescription = @Description("Repository dedicata a \"ProgrammingLanguage\" entity"))
-//
-//org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'welcomePageHandlerMapping' defined in class path resource
 public interface ProgrammingLanguageRepository extends JpaRepository<ProgrammingLanguage,Long> {
 }
